6942c75ab39a16178e8d199c28fdf10d1e5c8359
adil2133-git <adilaadi340@gmail.com>
2026-04-30 15:04:38 +0530

    cors updation

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://futegen-backend.onrender.com" + "/api",
   withCredentials: true
 });
 
