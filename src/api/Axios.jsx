4049c7778199e410046020c00e045c63416ebbf2
adil2133-git <adilaadi340@gmail.com>
2026-05-06 15:54:06 +0530

    api changed to local

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL + "/api",
+  baseURL: "http://localhost:5000/api",
   withCredentials: true
 });
 
